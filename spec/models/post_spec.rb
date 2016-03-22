65653d0e71e6d3bdef291f23fb72dce2107bcffe
Michael DeMarco <michael.demarco65@gmail.com>
2016-03-22 11:55:36 -0400

    Bloccit checkpoint 35 validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  it { is_expected.to belong_to(:topic) }
+
+  #validations
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "responds to title" do
