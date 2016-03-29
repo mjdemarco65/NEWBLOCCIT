c198b278738118d741ad061b76199375a4112281
Michael DeMarco <michael.demarco65@gmail.com>
2016-03-28 21:41:09 -0400

    Bloccati 39 users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,28 @@ require 'random_data'
 end
 topics = Topic.all
 
+# Create Users
+5.times do
+
+  User.create!(
+    name: RandomData.random_name,
+    email:  RandomData.random_email,
+    password:   RandomData.random_sentence
+  )
+end
+users = User.all
+
+user = User.first
+user.update_attributes!(
+  email: 'michael.demarco65@gmail.com',
+  password: 'helloworld'
+)
+
 # Create Posts
 50.times do
 
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -31,6 +49,7 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
