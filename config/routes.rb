cac1358e3481669575f7d129314a94666f091bfa
Michael DeMarco <michael.demarco65@gmail.com>
2016-06-22 16:12:13 -0400

    Labels 42 assignment

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] 
   resources :sessions, only: [:new, :create, :destroy]
+
 
 
   get 'about' => 'welcome#about'
